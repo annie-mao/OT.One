7df2fc7e9e3cf622440443648a8e9d44b9322cc1
Annie Mao <annie.m.mao@gmail.com>
2016-07-26 19:50:39 +0000

    Basic read/return/write working

--- otone_backend/backend/test_serial_write.py
+++ otone_backend/backend/test_serial_write.py
@@ -5,9 +5,21 @@ import time
 # use sudo raspi-config
 
 ser = serial.Serial('/dev/ttyUSB0',baudrate=9600,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
-print(ser.isOpen())
-ser.write('LID OPEN\r\n')
-time.sleep(10)
-ser.write('LID CLOSE\r\n')
+
+waiting = True
+resp = None
+ser.timeout=1
+ser.write('LID?\r\n')
+while waiting:
+    try:
+        resp=ser.read(1000)
+        if resp == 'OPEN\r\n':
+            waiting=False
+            ser.write('LID CLOSE\r\n')
+    except serial.SerialException:
+        pass
+#ser.write('LID OPEN\r\n')
+#time.sleep(10)
+#ser.write('LID CLOSE\r\n')
 
 ser.close
